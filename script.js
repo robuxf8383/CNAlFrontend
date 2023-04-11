c545e310f8f2a851611bc4377d10fed8e7e78196
coffa <130289346+robuxf8383@users.noreply.github.com>
2023-04-11 17:17:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,13 @@ menuIcon.addEventListener('click',showMenu);
 closeIcon.addEventListener('click',closeMenu);
 
 function showMenu(e){
-    // console.log('hello');
     let menuList=document.querySelector('.nav-menu');
     let appMenu = document.querySelector('.app-menu');
-    // appMenu.style.display = 'block';
     appMenu.style.transform='translateX(550px)';
 }
 
 function closeMenu(e){
     let appMenu=document.querySelector('.app-menu');
     let webMenu=document.querySelector('.web-menu')
-    // menuList.style.display = 'none';
     appMenu.style.transform='translateX(-550px)';
 }
